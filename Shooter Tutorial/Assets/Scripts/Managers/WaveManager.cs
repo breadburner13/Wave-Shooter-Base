39637dfebe1bd597c8f0e66570fe6cfe6a5eb36f
amiyourtype <35315317+amiyourtype@users.noreply.github.com>
2020-11-02 22:33:45 -0800

    edited wavemanager
    Kimi wa ne tashika ni ano toki watashi no soba ni ita
    Itsudatte itsudatte itsudatte sugu yoko de waratteita nakushitemo
    Torimodosu kimi wo I will never leave you.
    
    If you wanna battle, then I'll take it to the street
    Where there's no rules
    Take off the gloves ref, please step down
    Gotta prove my skills so get down
    
    My lyrical dempsey roll about to smack down now
    Gotta shoot to kill and shoot the skill
    Don't you be afraid, man's gotta do how it feels
    Six to seven to eight to nine ten
    I flip the script to make it to the top ten, go
    
    Dreamless dorm, ticking clock
    I walk away from the soundless room
    Windless night, moonlight melts
    My ghostly shadow into the lukewarm gloom
    
    Nightly dance of bleeding swords
    Reminds me that I still live
    
    Every man's gotta fight the fear
    I'm the first to admit it
    Sheer thoughts provoke the new era
    
    Become a big terror, but my only rival is my shadow
    Rewind then play it back and fix my own error
    Get low to the ground, it's getting better
    Like I told you before, double up and take more cheddar
    
    L to the J, say stay laced, here's my card, B
    Royal flush and I'm the ace

--- Shooter Tutorial/Assets/Scripts/Managers/WaveManager.cs
+++ Shooter Tutorial/Assets/Scripts/Managers/WaveManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WaveManager : MonoBehaviour
 {
 
-    public enum SpawnState {SPAWNING, WAITING, COUNTING };
+    public enum SpawnState {SPAWNING, WAITING, COUNTING};
 
     [System.Serializable]
     public class Wave
@@ -20,15 +20,16 @@ public class WaveManager : MonoBehaviour
     }
 
     public Wave[] waves;
-    private int nextwave = 0;
+    public int nextwave = 0;
+    public bool isContinuous;
 
     public Transform[] spawnpoints;
 
     public float timeBetweenWaves = 5f;
-    private float Wavecountdown;
+    public float Wavecountdown;
     public SpawnState state = SpawnState.COUNTING;
 
-    private float SearchCountDown = 1f;
+    public float SearchCountDown = 1f;
 
     private void Start()
     {
@@ -71,7 +72,7 @@ public class WaveManager : MonoBehaviour
         }
     }
 
-    void WaveCompleted()
+    public void WaveCompleted()
     {
         Debug.Log("Wave completed");
 
@@ -81,19 +82,32 @@ public class WaveManager : MonoBehaviour
 
         if (nextwave + 1 > waves.Length - 1)
         {
-            nextwave = 0;
-            Debug.Log("All waves complete... Looping");
+            if(isContinuous)
+            {
+                nextwave = 0;
+                Debug.Log("All waves complete... Looping");
+            }
+            else
+            {
+                nextwave = waves.Length + 1;
+                Debug.Log("Kimi wa ne tashika ni ano toki watashi no soba ni ita Itsudatte itsudatte itsudatte sugu yoko de waratteita nakushitemo"); 
+                Debug.Log("Torimodosu kimi wo I will never leave you. If you wanna battle, then Ill take it to the streets Where theres no rules Take off the gloves ref,"); 
+                Debug.Log("please step down Gotta prove my skills so get down My lyrical dempsey roll about to smack down now Gotta shoot to kill and shoot the skill Dont you be afraid,");
+                Debug.Log("mans gotta do how it feels Six to seven to eight to nine ten I flip the script to make it to the top ten, go Dreamless dorm, ticking clock I walk away from the");
+                Debug.Log("soundless room Windless night, moonlight melts My ghostly shadow into the lukewarm gloom Nightly dance of bleeding swords Reminds me that I still live Every mans ");
+                Debug.Log("gotta fight the fear Im the first to admit it Sheer thoughts provoke the new era Become a big terror, but my only rival is my shadow Rewind then play it back and");
+                Debug.Log("fix my own error Get low to the ground, its getting better Like I told you before, double up and take more cheddar L to the J, say stay laced, heres my card, B Royal flush and Im the ace");
+            }
         }
         else
         {
-
             nextwave++;
         }
         return;
     }
 
 
-    bool EnemyIsAlive()
+    public bool EnemyIsAlive()
     {
         SearchCountDown -= Time.deltaTime;
 
@@ -109,7 +123,7 @@ public class WaveManager : MonoBehaviour
         return true;
     }
 
-    IEnumerator SpawnWave(Wave _wave)
+    public IEnumerator SpawnWave(Wave _wave)
     {
         state = SpawnState.SPAWNING;
 
@@ -126,7 +140,7 @@ public class WaveManager : MonoBehaviour
         yield break;
     }
 
-    void SpawnEnemy (Transform _enemy)
+    public void SpawnEnemy (Transform _enemy)
     {
 
         Transform _sp = spawnpoints[Random.Range(0, spawnpoints.Length)];
@@ -135,5 +149,4 @@ public class WaveManager : MonoBehaviour
 
         Debug.Log("Spawning enemy");
     }
-
 }
